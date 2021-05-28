0274226c1eb3a023a6cd5ece5404ff57eff8a5e9
Simon Martiel <simon.martiel@atos.net>
2021-05-28 11:07:34 +0200

    Second attempt

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(
-    name="ejcim-2021",
-    description="Some notebooks for EJCIM 2021",
-    version="0.0.1",
-    install_requires=["myqlm==1.2.3"],
-)
