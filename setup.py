50f3efaaa535240584f2d9a3280fe42f8d067d8c
Simon Martiel <simon.martiel@atos.net>
2021-05-28 10:52:52 +0200

    First attempt with mybinder

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ejcim-2021",
+    description="Some notebooks for EJCIM 2021",
+    version="0.0.1",
+    install_requires=["myqlm"],
+)
